0d2ddb1e379c3c63a7b01016cb457ac7cab9d92a
Ammar Gomaa <64869779+ammargomaa1@users.noreply.github.com>
2024-10-19 19:50:34 +0300

    Time: 1 ms (92.79%) | Memory: 4.6 MB (6.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -2,30 +2,20 @@ func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-	for _, sLetter := range s {
-		tHasLetter := false
-		for tIndex, tLetter := range t {
-			if sLetter == tLetter {
-				tHasLetter = true
-				t = removeLetterFromString(tIndex, t)
-				break
-			}
-		}
+	charCount := map[rune] int{}
 
-		if !tHasLetter {
-			return tHasLetter
-		}
+    for _,char := range s{
+        charCount[char]++
+    }
 
-		s = s[1:]
+    for _,char := range t{
+        charCount[char]--
 
-	}
+        if charCount[char] < 0 {
+            return false
+        }
+    }
+    
 
 	return true
-}
-
-func removeLetterFromString(index int, s string) string {
-	if index < 0 || index >= len(s) {
-		return s // Index out of bounds, return original string
-	}
-	return s[:index] + s[index+1:]
 }
