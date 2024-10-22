b300a3f51aebcac00c2f7a4bffe6681c3e76c188
Ammar Gomaa <64869779+ammargomaa1@users.noreply.github.com>
2024-10-22 16:41:24 +0300

    Time: 17 ms (84.03%) | Memory: 8.9 MB (23.71%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,47 +1,21 @@
 func groupAnagrams(strs []string) [][]string {
     outPut := [][]string{}
 
-    chosenWords := map[string] bool{}
+    wordsMapped := map[string][]string{}
+    for _, word := range strs {
+        sortedWord := []rune(word)
 
-    for index,word := range strs {
-        angrams := []string{}
-        if _,found := chosenWords[word]; !found && len(angrams) == 0 {
-            angrams = append(angrams, word)
-        }else{
-            continue
-        }
+        sort.Slice(sortedWord,func(i,j int)bool{return sortedWord[i] < sortedWord[j]})
+        
+        sortedStr := string(sortedWord)
 
-        for i:= index+1; i<len(strs); i++ {
-            if isAngram(word, strs[i]){
-                 angrams = append(angrams, strs[i])
-                 chosenWords[strs[i]] = true
-            }
-        }
+        wordsMapped[sortedStr] = append(wordsMapped[sortedStr], word)
 
-        outPut = append(outPut, angrams)
+    }
+
+    for _, collectedWords := range wordsMapped {
+        outPut = append(outPut, collectedWords)
     }
 
     return outPut
-}
-
-func isAngram(firstWord string, secondWord string) bool {
-    if len(firstWord) != len(secondWord) {
-        return false
-    }
-
-    letterCounter := map[rune]int{}
-
-    for _,letter := range firstWord {
-        letterCounter[letter]++
-    } 
-
-    for _,letter2 := range secondWord {
-        letterCounter[letter2]--
-
-        if letterCounter[letter2] < 0 {
-            return false
-        }
-    } 
-
-    return true
 }
