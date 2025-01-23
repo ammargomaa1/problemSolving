47a9812ca6da4eedfb6a583ac28654e1d93b875d
Ammar Gomaa <64869779+ammargomaa1@users.noreply.github.com>
2025-01-23 19:00:19 +0200

    Time: 37 ms (5.46%) | Memory: 8.5 MB (5.46%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.go
@@ -0,0 +1,99 @@
+
+func isValidSudoku(board [][]byte) bool {
+	x := 0
+	for x < 9 {
+		isValid := isValideIndexRow(x, board)
+		if !isValid {
+			return false
+		}
+
+		isValid = validateIndexColumns(x, board)
+		if !isValid {
+			return false
+		}
+		x++
+	}
+
+	return true
+}
+
+func isValideIndexRow(index int, board [][]byte) bool {
+	row := board[index]
+	foundNums := make(map[byte]bool, 9)
+
+	for _, num := range row {
+		if num == 46 {
+			continue
+		}
+
+		if !foundNums[num] {
+			foundNums[num] = true
+		} else {
+			return false
+		}
+	}
+
+	return validateQuarters(board)
+}
+
+func validateIndexColumns(index int, board [][]byte) bool {
+	foundNums := make(map[byte]bool, 9)
+
+	for _, row := range board {
+		if row[index] == 46 {
+			continue
+		}
+
+		if !foundNums[row[index]] {
+			foundNums[row[index]] = true
+		} else {
+			return false
+		}
+
+	}
+
+	return true
+}
+
+func validateQuarters(board [][]byte) bool {
+	sets := map[int][]byte{}
+	rowSet := 1
+	for rowIndex, row := range board {
+		columnSet := 1
+		for columnIndex, column := range row {
+			setName := ((rowSet) * 10) + (columnSet)
+			sets[setName] = append(sets[setName], column)
+			if (columnIndex+1)%3 == 0 {
+				columnSet++
+			}
+
+		}
+
+		if (rowIndex+1)%3 == 0 {
+			rowSet++
+		}
+	}
+
+	fmt.Println(sets)
+
+	foundNums := make(map[byte]bool, 9)
+
+	for _, set := range sets {
+
+		for _, num := range set {
+			if num == 46 {
+				continue
+			}
+
+			if !foundNums[num] {
+				foundNums[num] = true
+			} else {
+				return false
+			}
+		}
+		foundNums = make(map[byte]bool, 9)
+
+	}
+	return true
+
+}
